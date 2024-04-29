5cd48621c8cd3c6cfe91c25f1e49b744ab0c3e91
a23lisjo <a23lisjo@student.his.se>
2024-04-29 12:05:18 +0200

    Lagt till textview som kan visa text från "EditText" Lagt till en on click listener på knappen som byter aktivitet

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,55 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    // in My MainActivity class
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // In onCreate()
+        myPreferenceRef = getSharedPreferences ( "preference" ,MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Read a preference
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No pref"));
+
+        Button changeAct=findViewById(R.id.changeAct);
+        changeAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+
 }
+
+
+// In onCreate()
+
+
