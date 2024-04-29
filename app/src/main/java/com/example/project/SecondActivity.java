7cd65e16e33188e2ae84e002ba96b373df692b15
a23lisjo <a23lisjo@student.his.se>
2024-04-29 12:06:51 +0200

    Lagt till savePref funktion som sparar string i "EditText". Lagt till en intent som går tillbaka till MainActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,8 +1,48 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SecondActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // In onCreate()
+        myPreferenceRef = getSharedPreferences("preference",MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
 
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+    public void savePref(View back){
+        // Get the text
+        EditText newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.settingseditview);
+
+        // Store the new preference
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        // Clear the EditText
+        newPrefText.setText("");
+
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
